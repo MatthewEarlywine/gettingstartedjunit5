a224b8fbebe70e7f2851aaf4fdde2a76ec02e18d
matthew.earlywine <matthew.earlywine@AFSWWEN80SMUQN.us.accenturefederal.com>
2022-08-15 15:02:49 -0500

    Added lifecycle Annotations

--- test/patientintake/ClinicCalendarShould.java
+++ test/patientintake/ClinicCalendarShould.java
@@ -1,5 +1,9 @@
 package patientintake;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -10,10 +14,23 @@ import java.util.Locale;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClinicCalendarShould {
+	
+	private ClinicCalendar calendar;
+
+	@BeforeAll
+	static void testClassSetup() {
+		System.out.println("Before all...");
+	}
+	
+	@BeforeEach
+	void init() {
+		System.out.println("Before each...");
+		calendar = new ClinicCalendar(LocalDate.of(2022, 8, 15));
+	}
 
    @Test
    void allowEntryOfAnAppointment() {
-      ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+	  System.out.println("entry of appointment...");
       calendar.addAppointment("Jim", "Weaver", "avery",
          "09/01/2018 2:00 pm");
       List<PatientAppointment> appointments = calendar.getAppointments();
@@ -29,23 +46,34 @@ class ClinicCalendarShould {
    
    @Test
    void returnTrueForHasAppointmentsIfThereAreAppointments() {
-	   ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+	   System.out.println("has appointments...");
 	   calendar.addAppointment("Jim", "Weaver", "avery", "09/01/2018 2:00 pm");
 	   assertTrue(calendar.hasAppointment(LocalDate.of(2018, 9, 1)));
    }
    
    @Test
    void returnFalseForHasAppointmentsIfThereAreNoAppointments() {
-	   ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+	   System.out.println("no appointments...");
 	   assertFalse(calendar.hasAppointment(LocalDate.of(2018, 9, 1)));
    }
    
    @Test
    void returnCurrentDaysAppointments() {
-	   ClinicCalendar calendar = new ClinicCalendar(LocalDate.of(2022, 8, 15));
+	   System.out.println("Current day's appointments...");
 	   calendar.addAppointment("Jim", "Weaver", "avery", "08/15/2022 2:00 pm");  //make sure these are set to
 	   calendar.addAppointment("Jim", "Weaver", "avery", "08/15/2022 3:00 pm");  //today's date before running tests.
 	   calendar.addAppointment("Jim", "Weaver", "avery", "12/15/2018 2:00 pm");
 	   assertEquals(2, calendar.getTodayAppointments().size());
+   }
+   
+   @AfterEach
+   void tearDownEachTest() {
+	   System.out.println("After each...");
+	   
+   }
+   
+   @AfterAll
+   static void tearDownTestClass() {
+	   System.out.println("After all...");
    }
 }
